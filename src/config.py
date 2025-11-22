ce3115dc29f60db2e8d4d2eb67765ebc2d5a6754
skeletorflet <ositomalvado.art1@gmail.com>
2025-11-22 20:07:46 -0300

    g

--- src/config.py
+++ src/config.py
@@ -1,2 +1,2 @@
-# A1111_URL = "http://localhost:7860"
-A1111_URL = "https://8c4ba0aba0a5052ca1.gradio.live"
+A1111_URL = "http://localhost:7860"
+# A1111_URL = "https://8c4ba0aba0a5052ca1.gradio.live"
